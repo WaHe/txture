14347d3b11dcc30344a4b3d2d294807882ef9778
Walker Henderson <wjahenderson@gmail.com>
2015-02-28 01:26:30 -0600

    moved routes into init

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from flask.ext.sqlalchemy import SQLAlchemy
-
 from secrets import database_url
+import twilio.rest
+import twilio.twiml
+import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
@@ -42,7 +44,37 @@ class Address(db.Model):
         self.user_phone_number = user_phone_number
 
 
-import routes
+@app.route("/")
+def index():
+    db.session.add(User(str(random.random())))
+    db.session.commit()
+    return "Hello, I love Digital Ocean!"
+
+
+@app.route("/users")
+def users():
+    uses = User.query.all()
+    return str(uses)
+
+
+@app.route("/twiltest")
+def twiltest():
+    # Find these values at https://twilio.com/user/account
+    client = twilio.rest.TwilioRestClient(app.twilio_sid, app.twilio_token)
+    message = client.messages.create(to="+14157862965", from_=app.twilio_number,
+                                     body="Hello there!")
+    return "sent a message!"
+
+
+@app.route("/tw", methods=['POST'])
+def twilio_route():
+    """Respond to incoming calls with a simple text message."""
+    from_number = request.values.get('From', None)
+    resp = twilio.twiml.Response()
+    resp.message("Hello, you! That was from " + from_number)
+    resp.message("here's another message")
+    return str(resp)
+
 
 if __name__ == "__main__":
     app.run()
